9f56c3a7277dad09743560d0bce05638498b0a1b
Pritesh Chandra <priteshchandra2223@gmail.com>
2024-05-26 19:00:07 +0530

    adding links

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,12 +5,12 @@ const Header = () => {
     <header>
         <div className='header-main'>
             <div className='header-left-content'>
-                <h1><a href='/'>LogSwift</a></h1>
+                <h1><a href='/log-swift-webapp'>LogSwift</a></h1>
             </div>
             <div className='header-right-content'>
                 <ul>
                     <li>
-                       <a href='/'>Home</a> 
+                       <a href='/log-swift-webapp'>Home</a> 
                     </li>
                     <li>
                        <a href='https://github.com/pritesh-chandra/log-swift/blob/main/README.md' target="_blank" rel="noopener noreferrer">Docs</a> 
